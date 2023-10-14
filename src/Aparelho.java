b16a082c8ce1c751bf240a665ac1694fb9fc936c
Luis Fernando <lfm.1383@outlook.com>
2023-10-14 14:57:05 -0300

    Classes Iphone

---
+++ src/Aparelho.java
@@ -0,0 +1,7 @@
+public interface Aparelho {
+    public void ligar(String numero);
+
+    public void atender(String numero);
+
+    public void iniciarCorreioVoz(String mensagem, String destinatario);
+}
---
+++ src/InternetNavegacao.java
@@ -0,0 +1,8 @@
+public interface InternetNavegacao {
+
+    public void exibirPagina(String url);
+
+    public void adicionarNovaAba(String url);
+
+    public void atualizarPagina();
+}
